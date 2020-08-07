f54f15e96dca89cfcacbc8285efd7cb932fd80bf
felipe <silas.garcia@mfconsulting.com.br>
2020-08-06 23:14:14 -0300

    utilizando metodo onchange e usando slice

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhotoModel } from 'src/app/model/photo-model';
 
 @Component({
@@ -6,13 +6,31 @@ import { PhotoModel } from 'src/app/model/photo-model';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: PhotoModel[];
 
+  rows : any = [];
+
   constructor() { }
 
-  ngOnInit() {
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes.photos){
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+
+  groupColumns(photos: PhotoModel[]){
+    const newRows = [];
+
+    console.log(photos);
+    for(let index = 0; index < photos.length; index+=3){
+      newRows.push(photos.slice(index,index+3));
+    }
+    console.log(newRows);
+    return newRows;
   }
 
 }

c16ab29526b87d0dc650231025f8884f72ddca45
felipe <silas.garcia@mfconsulting.com.br>
2020-08-17 22:33:54 -0300

    criando componente ap-load-button e colocando na pagina photo-list

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,6 +7,7 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotosListComponent } from './photos-list/photos-list.component';
 import { PhotosFormComponent } from './photos-form/photos-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
     declarations: [ 
@@ -14,7 +15,8 @@ import { PhotosComponent } from './photo-list/photos/photos.component';
         PhotosListComponent, 
         PhotosFormComponent, 
         PhotosComponent,
-        FilterByDescription
+        FilterByDescription,
+        LoadButtonComponent
     ],
     imports: [ 
         HttpClientModule,

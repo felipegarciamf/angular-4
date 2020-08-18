10bbd862b5a084002b4367121f402b139a7363e0
felipe <silas.garcia@mfconsulting.com.br>
2020-08-17 22:17:24 -0300

    ativando unsubscribe assim que a pagina sair, para nao alocar na memoria o uso deste observable

--- src/app/photos/photos-list/photos-list.component.ts
+++ src/app/photos/photos-list/photos-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
@@ -11,20 +11,27 @@ import { PhotoModel } from './../../model/photo-model';
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
+export class PhotosListComponent implements OnInit, OnDestroy {
 
   public photos: PhotoModel[] = [];
   public filter: string = '';
-  debouce: Subject<string> = new Subject<string>();
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {
   }
 
+
   ngOnInit() {
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debouce
+    this.debounce
     .pipe(debounceTime(300))
     .subscribe(filter => this.filter = filter);
   }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+
 
 }

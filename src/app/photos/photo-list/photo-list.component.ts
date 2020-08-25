91fed696f6e71f1845a215870e97c463cf42f4bf
felipe <silas.garcia@mfconsulting.com.br>
2020-08-25 00:01:56 -0300

    corrigindo problema do findmore para limpar o search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -37,6 +37,7 @@ export class PhotosListComponent implements OnInit {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => { 
+        this.filter = '';
         this.photos = this.photos.concat(photos);
         if(!photos.length) this.hasMore = false;
       });
